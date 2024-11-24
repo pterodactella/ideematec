6162f28b766240cf1dcc011a8cd224a52674cf11
Daniela <daniela.bahneanu@gmail.com>
2024-11-24 15:49:54 +0100

    chore: refactor the control panel class

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ class ControlPanel(QtWidgets.QWidget):
         self.init_ui()
 
     def init_ui(self):
+        """
+        This function sets up the control widgets: dropdown with the shapes,
+        input fields and button to save the shape information.
+        """
         # Create drop-down menu for choosing the shape
         shape_dropdown_label = QtWidgets.QLabel("Shape:")
         self.shape_dropdown = QtWidgets.QComboBox()
         self.shape_dropdown.addItems(["SQR", "U"])
 
-        # Create Input fields for each shape
+        # Create input fields
         self.side_label = QtWidgets.QLabel("Side:")
         self.side_input = QtWidgets.QLineEdit()
         self.side_input.setValidator(QtGui.QDoubleValidator(0.0, 1000.0, 2))
@@ -49,17 +53,15 @@ class ControlPanel(QtWidgets.QWidget):
             lambda: self.format_input(self.thickness_input)
         )
 
-        # Function to update input fields based on dropdown selection
+        # Conditionally render the fields corresponging to the selected shape
         self.shape_dropdown.currentIndexChanged.connect(self.conditional_rendering)
-
-        # Call the function once to set the initial state
         self.conditional_rendering()
 
-        # Add a button to draw the shape
+        # Add a button to draw and save the shape
         draw_button = QtWidgets.QPushButton("Draw Shape")
         draw_button.clicked.connect(self.draw_shape)
 
-        # Add the label and component in a form layout
+        # Add the labels
         form = QtWidgets.QFormLayout()
         form.addRow(shape_dropdown_label, self.shape_dropdown)
         form.addRow(self.side_label, self.side_input)
@@ -71,23 +73,28 @@ class ControlPanel(QtWidgets.QWidget):
         self.setLayout(form)
 
     def conditional_rendering(self):
-        shape_type = self.shape_dropdown.currentText()
-        if shape_type == "SQR":
-            self.side_label.setVisible(True)
-            self.side_input.setVisible(True)
-            self.height_label.setVisible(False)
-            self.height_input.setVisible(False)
-            self.width_label.setVisible(False)
-            self.width_input.setVisible(False)
-        elif shape_type == "U":
-            self.side_label.setVisible(False)
-            self.side_input.setVisible(False)
-            self.height_label.setVisible(True)
-            self.height_input.setVisible(True)
-            self.width_label.setVisible(True)
-            self.width_input.setVisible(True)
+        """
+        Create mapping of input field for given shape
+        """
+        field_visibility = {
+            "SQR": {"side": True, "height": False, "width": False},
+            "U": {"side": False, "height": True, "width": True},
+        }
+
+        visibility = field_visibility.get(self.shape_dropdown.currentText(), {})
+        self.side_label.setVisible(visibility.get("side", False))
+        self.side_input.setVisible(visibility.get("side", False))
+        self.height_label.setVisible(visibility.get("height", False))
+        self.height_input.setVisible(visibility.get("height", False))
+        self.width_label.setVisible(visibility.get("width", False))
+        self.width_input.setVisible(visibility.get("width", False))
 
     def draw_shape(self):
+        """
+        This function draws the shapes depending of the user selection
+        """
+
+        # Clear the previous drawing
         self.canvas.clear_scene()
 
         shape_type = self.shape_dropdown.currentText()
@@ -103,10 +110,14 @@ class ControlPanel(QtWidgets.QWidget):
             channel = Channel(height=height, width=width)
             drawn_shape = DrawnShape(shape=channel, thickness=thickness)
 
+        # Draw and save
         self.canvas.shape_drawer.draw_shape(drawn_shape)
         self.save_shape(drawn_shape)
 
     def save_shape(self, shape: DrawnShape):
+        """
+        This function saves the DrawnShape model inside a json file.
+        """
         array = shape.to_array()
         try:
             with open("store.json", "r") as f:
@@ -120,8 +131,11 @@ class ControlPanel(QtWidgets.QWidget):
         with open("store.json", "w") as f:
             json.dump(shapes, f, indent=4)
 
-    # One of the issues that i noticed was that when having a field in and out of focus the value would get rewritten in a scientific notation, that why we have this function.
     def format_input(self, line_edit):
+        """
+        When having a field in and out of focus the value would get rewritten
+        in a scientific notation, this function insures consistent formatting.
+        """
         value = line_edit.text()
         if value:
             line_edit.setText(f"{float(value):.2f}")
